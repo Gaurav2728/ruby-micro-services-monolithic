3679942186c867a9592eefb4e50ccbd7cc0f6b9c
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-01-31 10:57:16 -0800

    Email user after they have been added to system * Use Letter Opener in dev * Hook after_create in User model, sending through UserMailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  after_create :notify_added
+
+private
+  def notify_added
+    UserMailer.notify_added(self).deliver
+  end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,9 @@ Monolithic::Application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  # Use Letter Opener for previewing emails
+  config.action_mailer.delivery_method = :letter_opener
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
